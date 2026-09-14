c6e859159bd16bcdc697e09e9b6be2d0f7df5a7c
MahaManisha <mahamanisha2005@gmail.com>
2026-09-14 15:02:48 +0530

    STEVES0001 - Added utility function for addition

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,11 @@
+public class Utility {
+
+    public static void steve() {
+        System.out.println("I am Steve");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+}
